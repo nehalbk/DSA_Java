86b9a19e6646314349db1d3e8f5076256b4e570c
Nehal Kankanawadi <nehalbk333@gmail.com>
2023-11-22 23:11:37 +0530

    Added WRapperClass description

--- src/WrapperClasses.java
+++ src/WrapperClasses.java
@@ -32,17 +32,18 @@ public class WrapperClasses {
 		//Integer is a wrapper class and the Integer class keeps a cache of Integer instances in the range of -128 to 127
 		System.out.println(a==b); //true
 		System.out.println(c==d); //false
-		
+		System.out.println();
 		//Character is a wrapper class and the Character class keeps a cache of basic Character instances (alphabets, letters, punctuation)
-		//but not unusual special characters
+		//but not unusual special characters (from ASCII 0 to 127)
 		System.out.println(e==f); //true
 		System.out.println(g==h); //false
 		System.out.println(i==j); //true
-		
+		System.out.println();
 		//Every string creates a memory reference if new strings are created with same value they are pointed to same reference hence they 
 		// are always equal
 		System.out.println(k==l);
 		System.out.println(m==n);
+		System.out.println();
 		
 	}
 
