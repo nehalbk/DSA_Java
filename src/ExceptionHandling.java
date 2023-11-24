9ceabf777b018b187eff295386d7dae963dd07bf
Nehal Kankanawadi <nehalbk333@gmail.com>
2023-11-24 19:47:04 +0530

    Added explaintation comments for ExceptionHandling program

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -1,11 +1,24 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 class checkNum {
 	
 	public static double ans(int n) throws Exception{
-		if(n==0) throw new RuntimeException(" Not divisible by 0!",new IllegalArgumentException("0 was passed"));
-		if(n==1) throw new IOException("1 is not right thing to pass!");
-		if(n==2) throw new Exception("2 was passed!");
+		if(n==0) 
+			//Unchecked exception with cause as parameter
+			//We encountered Runtime Exception. This exception was caused due to IllegalArgumentException
+			throw new RuntimeException(" Not divisible by 0!",new IllegalArgumentException("0 was passed")); 
+		if(n==1) 
+			//Checked Exception checked during compile time
+			//only message is sent and cause is not known hence only q parameter
+			throw new IOException("File 1 not found!"); 
+		if(n==2) 
+			//Checked Exception checked during compile time
+			//throws IOException i.e Input Output Exception, cause is File not found
+			throw new IOException("Error opening file!", new FileNotFoundException("File not found!")); 
+		if(n==3) 
+			//Generic Exception with no special type or cause
+			throw new Exception("3 was passed!");	
 		return 50/n;
 	}
 
@@ -15,16 +28,36 @@ public class ExceptionHandling {
 	
 	public static void main(String[] args) {
 	try {
-		System.out.println(checkNum.ans(01));
-	} catch (IOException e) {
-		System.out.println(e.getMessage());
+		System.out.println(checkNum.ans(02));
+	} catch (IOException e) {						//Catch specific error first
+		System.out.println(e.getCause().getMessage()); //To show user
+		System.out.println(e.getCause());			//for developers
 		e.printStackTrace();
-	} catch (RuntimeException  e) {
+	} catch (RuntimeException  e) {				//Catch higher order second specific error second
 		e.printStackTrace();
 		System.out.println(e.getCause().getMessage());
 	} catch(Exception e) {
-		System.out.println(e.getMessage());
+		System.out.println(e.getMessage());		//Catch generalized error last
 	}
 	
 	}
 }
+
+/*
+ * Link to Refer all exceptions - https://docs.oracle.com/javase/8/docs/api/java/lang/Exception.html
+  	
+ 	Parameters for Exception:
+ 	
+	Exception(String message,
+	                    Throwable cause,
+	                    boolean enableSuppression,
+	                    boolean writableStackTrace)
+	                    
+	Constructs a new exception with the specified detail message, cause, suppression enabled or disabled, and writable stack trace enabled or disabled.
+	Parameters:
+	
+	message - the detail message.
+	cause - the cause. (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+	enableSuppression - whether or not suppression is enabled or disabled
+	writableStackTrace - whether or not the stack trace should be writable
+ */
