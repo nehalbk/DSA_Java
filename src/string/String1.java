8867d35488d912902c28d83c46d9601743e2013d
Nehal Kankanawadi <nehalbk333@gmail.com>
2023-12-13 21:37:42 +0530

    Added leet code string problem 1108 solution

---
+++ src/string/String1.java
@@ -0,0 +1,36 @@
+/*
+ * Question : [Source - Leetcode : https://https://leetcode.com/problems/defanging-an-ip-address/]
+ * 
+ * 1108. Defanging an IP Address
+ * 
+ * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+	A defanged IP address replaces every period "." with "[.]".
+	
+	 
+	
+	Example 1:
+	
+	Input: address = "1.1.1.1"
+	Output: "1[.]1[.]1[.]1"
+	Example 2:
+	
+	Input: address = "255.100.50.0"
+	Output: "255[.]100[.]50[.]0"
+ */
+
+package string;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		System.out.println(defangIPaddr("1.1.1.1"));
+
+	}
+    public static String defangIPaddr(String address) {
+    	
+        return address.replace(".", "[.]");
+    }
+
+
+}
