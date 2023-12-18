7168fb27eaff996a91b84516783ac285fe7b2190
Nehal Kankanawadi <nehalbk333@gmail.com>
2023-12-18 21:59:06 +0530

    Added another approach

--- src/stack/Stack1.java
+++ src/stack/Stack1.java
@@ -39,19 +39,38 @@ public class Stack1 {
 
 	}
 	
-	public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+public static boolean isValid(String s) {
+  Stack<Character> stack = new Stack<>();
 
-        for(char c: s.toCharArray()){
-            if(c=='(' || c=='['||c=='{') stack.push(c);
-            else if(stack.isEmpty()) return false;
-            else if(c==')' && stack.pop()!='(') return false;
-            else if(c==']' && stack.pop()!='[') return false;
-            else if(c=='}' && stack.pop()!='{') return false;
+  for(char c: s.toCharArray()){
+      if(c=='(' || c=='['||c=='{') stack.push(c);
+      else if(stack.isEmpty()) return false;
+      else if(c==')' && stack.pop()!='(') return false;
+      else if(c==']' && stack.pop()!='[') return false;
+      else if(c=='}' && stack.pop()!='{') return false;
 
-        }
-        
-        return stack.isEmpty();
-    }
+  }
+  
+  return stack.isEmpty();
+}
+	
+//	public static boolean isValid(String s) {
+//        Stack<Character> stack = new Stack<>();
+//
+//        for(char c: s.toCharArray()){
+//            switch(c) {
+//            case '(': stack.push(')'); break;
+//            case '[': stack.push(']'); break;
+//            case '{': stack.push('}'); break;
+//            
+//            default: if(stack.isEmpty() || stack.pop()!=c) return false;
+//            }
+//
+//        }
+//        
+//        return stack.isEmpty();
+//    }
+	
+
 
 }
