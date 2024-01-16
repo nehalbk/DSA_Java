94b684063f85806391fd8a106b02dd390236c34e
Nehal Kankanawadi <nehalbk333@gmail.com>
2024-01-16 23:16:15 +0530

    Changed description of Arrays 5

--- src/Arrays/Arrays5.java
+++ src/Arrays/Arrays5.java
@@ -1,9 +1,9 @@
 /*
- * Question : [Source - Leetcode : https://leetcode.com/problems/pascals-triangle/description/]
+ * Question : [Source - Leetcode : https://leetcode.com/problems/pascals-triangle-ii/description/]
  * 
- * 118. Pascal's Triangle
-	
-	Given an integer numRows, return the first numRows of Pascal's triangle.
+ * 119. Pascal's Triangle II
+ * 
+	Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
 	
 	In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 	
@@ -12,17 +12,24 @@
 	
 	Example 1:
 	
-	Input: numRows = 5
-	Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+	Input: rowIndex = 3
+	Output: [1,3,3,1]
 	Example 2:
 	
-	Input: numRows = 1
-	Output: [[1]]
+	Input: rowIndex = 0
+	Output: [1]
+	Example 3:
+	
+	Input: rowIndex = 1
+	Output: [1,1]
 	 
 	
 	Constraints:
 	
-	1 <= numRows <= 30
+	0 <= rowIndex <= 33
+	 
+	
+	Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
 */
 		
 	package Arrays;
